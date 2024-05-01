69496cceee1f265ae6797d223a6f2110a0745333
hanzhifeng <13718536806@163.com>
2024-05-01 17:48:03 +0800

    feature# 处理首页

--- eso-eso_from_gitlab/lib/page/recommand/recommand_page.dart
+++ eso-eso_from_gitlab/lib/page/recommand/recommand_page.dart
@@ -1,8 +1,10 @@
+import 'package:eso/page/recommand/widget/hot_recommend_item.dart';
 import 'package:eso/page/recommand/widget/product_item_widget.dart';
 import 'package:eso/utils/org_color_utils.dart';
 import 'package:flutter/material.dart';
 
 import 'entity/product_item.dart';
+
 class RecommendPage extends StatefulWidget {
   const RecommendPage({Key key}) : super(key: key);
 
@@ -15,59 +17,108 @@ class _RecommendPageState extends State<RecommendPage> {
   @override
   void initState() {
     itemList = [
-
-          ProductItem("王者荣耀", "img", "腾讯"),
-          ProductItem("吃鸡", "img", "ali"),
-          ProductItem("原神", "img", "腾讯"),
-        ProductItem("王者荣耀", "img", "iOS"),
-        ProductItem("吃鸡", "img", "Android"),
-        ProductItem("原神", "img", "Flutter"),
-
+      ProductItem("王者荣耀", "img", "腾讯"),
+      ProductItem("吃鸡", "img", "ali"),
+      ProductItem("原神", "img", "腾讯"),
+      ProductItem("王者荣耀", "img", "iOS"),
+      ProductItem("吃鸡", "img", "Android"),
+      ProductItem("原神", "img", "Flutter"),
     ];
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, index) {
-      if (index == 0) {
-        return Container(
-          margin: const EdgeInsets.all(16),
-          height: 374,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(15)),
-          ),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 10,right: 10,top: 10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("猜你喜欢",style: TextStyle(fontSize: 16,color: Colors.black),),
-                    Text("换一换",style: TextStyle(fontSize: 14,color: ColorsUtil.contractColor("#86909C")),),
-                  ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        if (index == 0) {
+          return Container(
+            margin: const EdgeInsets.all(16),
+            height: 374,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.only(left: 10, right: 10, top: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "猜你喜欢",
+                        style: TextStyle(fontSize: 16, color: Colors.black),
+                      ),
+                      Text(
+                        "换一换",
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: ColorsUtil.contractColor("#86909C")),
+                      ),
+                    ],
+                  ),
                 ),
+                Expanded(
+                  child: GridView(
+                    physics: NeverScrollableScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 155 / 93,
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10,
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 10,
+                    ),
+                    children: itemList
+                        .map((e) => ProductItemWidget(
+                              item: e,
+                            ))
+                        .toList(),
+                  ),
+                )
+              ],
+            ),
+          );
+        } else {
+          return Container(
+            width: double.infinity,
+            margin: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.white,
+            ),
+            child: Expanded(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 10, right: 10, top: 10.0),
+                    child: Text("热门推荐",style: TextStyle(fontSize: 16, color: Colors.black)),
+                  ),
+                  Expanded(
+                      child: GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 1/2,
+                        mainAxisSpacing: 10,
+                        crossAxisSpacing: 10
+                      ), itemBuilder: (context, index) {
+                        return HotRecommendItem();
+                      },
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: 30,
+                      )
+                  ),
+                ],
               ),
-              Expanded(
-                child: GridView(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                  padding: EdgeInsets.symmetric(vertical: 8,horizontal: 10,),
-                  children: itemList.map((e) => ProductItemWidget(item: e,)).toList(),
-                ),
-              )
-            ],
-          ),
-        );
-      } else {
-        return Container(
-          height: 40,
-          width: double.infinity,
-        );
-      }
-      return Container();
-    },
+            ),
+          );
+        }
+      },
       itemCount: 2,
     );
   }
--- eso-eso_from_gitlab/lib/page/recommand/widget/product_item_widget.dart
+++ eso-eso_from_gitlab/lib/page/recommand/widget/product_item_widget.dart
@@ -4,33 +4,39 @@ import 'package:flutter/material.dart';
 import '../entity/product_item.dart';
 class ProductItemWidget extends StatelessWidget {
   ProductItem item;
-  ProductItemWidget({Key key,this.item}) : super(key: key);
+  ProductItemWidget({Key key, this.item}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      // color: Colors.red,
-      // width: 130,
+      color: Colors.white,
       // height: 73,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Expanded(
-            child: Container(
-
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: Colors.yellow,
-              ),
-              // child: Icon(Icons.alarm),
+          Container(
+            margin: const EdgeInsets.all(8.0),
+            width: 65,
+            height: 83,
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Image.network(
+              "https://img1.baidu.com/it/u=3593114214,976018682&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500",
+              fit: BoxFit.cover,
             ),
           ),
-          Column(
-            children: [
-              Text(item.name,style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500,color: Colors.black),),
-              const SizedBox(height: 4,),
-              Text(item.author,style: TextStyle(fontSize: 14,color: ColorsUtil.contractColor("#A6A6A6")),),
-            ],
+          Padding(
+            padding: const EdgeInsets.only(top: 16.0,right: 8.0,bottom: 8.0),
+            child: Column(
+              children: [
+                Text(item.name,style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500,color: Colors.black),),
+                const SizedBox(height: 4,),
+                Text(item.author,style: TextStyle(fontSize: 14,color: ColorsUtil.contractColor("#A6A6A6"))),
+              ],
+            ),
           ),
         ],
       ),
